5ae52b48fab1f6e5cd4c2efe172574ede79a5be0
brand445566777 <arshaddawood04@gmail.com>
2026-07-05 19:38:59 +0500

    Permanently fixed minifier displayName crash

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,12 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.watchFolders = [__dirname];
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: {
+    keep_fnames: true,
+  },
+};
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = config;
